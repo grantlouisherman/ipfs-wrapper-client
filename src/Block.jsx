60ee774c42cb5eb08e0ac1645158436a66a14b78
Grant Herman <grantlouisherman041@gmail.com>
2019-04-16 17:03:01 -0700

    feat(block): have block service working, with cb hell

--- src/Block.jsx
+++ src/Block.jsx
@@ -1,37 +1,88 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-const BlockService = require('ipfs-block-service')
-const Block_ = require('ipfs-block')
-const multihashing = require('multihashing-async')
-const IPFSRepo = require('ipfs-repo')  // storage repo
+import CID from 'cids'
+import BlockService from 'ipfs-block-service'
+import Block from 'ipfs-block'
+import multihashing from 'multihashing-async'
+import IPFSRepo from 'ipfs-repo'  // storage repo
 
-// import CID from 'cids'
 
-class Block extends Component {
+class Blocks extends Component {
   constructor(){
     super()
     this.state = {
-      data: ''
+      data: '',
+      bs: '',
+      block: ''
     }
+    this.createData = this.createData.bind(this)
+    this.createBlock = this.createBlock.bind(this)
+  }
+  
+  createData(type, blockData){
+    return new Buffer("HELLO")
+  }
+  createBlock(data, multihash){
+    const cid = new CID(multihash)
+    const block = new Block(data, cid)
+    return { cid, block }
+  }
+
+  componentDidMount(){
+    const { blockData, type } = this.props
+    const data = this.createData(type, blockData)
+    // setup a repo
+    const repo = new IPFSRepo('example')
+    repo.init({ cool: 'config' }, (err) => {
+      if (err) {
+        throw err
+      }
+      repo.open((err) => {
+        if (err) {
+          throw err
+        }
+        multihashing(data, 'sha2-256', (err, multihash) => {
+          if (err) {
+            throw err
+          }
+          const getBlock = this.createBlock(data, multihash)
+          const block = getBlock.block
+          const cid = getBlock.cid
+
+          // create a service
+          const bs = new BlockService(repo)
+          // add the block, then retrieve it
+          bs.put(block, (err) => {
+            if (err) {
+              console.log(err)
+            }
+            bs.get(cid, (err, b) => {
+              if (err) {
+                throw err
+              }
+              this.setState({data:block.data.toString()})
+            })
+          })
+        })
+        })
+    })
+
   }
   render(){
-    const { children, type } = this.props;
     const { data } = this.state
-    return this.props.children(this.state)
+    return this.props.children({data})
   }
 
 }
 
 Block.propTypes = {
   type: PropTypes.string,
-  cid: PropTypes.string,
-  block: PropTypes.string
+  blockData: PropTypes.string
 }
 
 Block.defaultProps = {
-  type: 'put',
-  cid: '',
-  block: ''
+  type: 'string',
+  blockData: 'HELLO',
 }
 
-export default Block
+export default Blocks
